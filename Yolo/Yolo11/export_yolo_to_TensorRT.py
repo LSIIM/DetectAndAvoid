22f7b4dab9f87c0d73e946fcae0f2210ee9d6403
vinicius <105316221+vini-muchulski@users.noreply.github.com>
2026-05-09 13:21:14 -0300

    Fix model path in export_yolo_to_TensorRT.py

--- Yolo/Yolo11/export_yolo_to_TensorRT.py
+++ Yolo/Yolo11/export_yolo_to_TensorRT.py
@@ -1,17 +1,14 @@
 from ultralytics import YOLO
 
-model = YOLO(r'/Yolo/Yolo11/Weights/best_yolo_11_JUNHO_nano_drones_DGX.pt')
+model = YOLO(r'Weights/best_yolo_11_JUNHO_nano_drones_DGX.pt')
 
 model.export(
     format='engine',
     device=0,
-    
     half=True,
-    #int8=True,
     batch=4,
     imgsz=640,
     dynamic=True
-    #workspace=4
 )
 
 print("Exportação para TensorRT (.engine) concluída!")
